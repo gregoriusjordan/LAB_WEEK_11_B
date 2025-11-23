3bc5dd6f1ca6cad1bb6f7f2351b2b81e74ea388a
Gregorius Jordan <gregoriusjordan@gmail.com>
2025-11-24 02:15:04 +0700

    Commit No. 2: Photo and Video using FileProvider and MediaStore

--- app/src/main/java/com/example/lab_week_11_b/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_11_b/MainActivity.kt
@@ -1,20 +1,74 @@
 package com.example.lab_week_11_b
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.MediaStore
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.util.Date
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var providerManager: ProviderFileManager
+    private var latestTmpUri: FileInfo? = null
+
+    private val takePicture = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult())
+    { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            providerManager.insertImageToStore(latestTmpUri)
+        }
+    }
+
+    private val recordVideo = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) 
+    { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            providerManager.insertVideoToStore(latestTmpUri)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        providerManager = ProviderFileManager(
+            this,
+            FileHelper(this),
+            contentResolver,
+            Executors.newSingleThreadExecutor(),
+            MediaContentHelper()
+        )
+
+        findViewById<Button>(R.id.photo_button).setOnClickListener {
+            takePicture()
+        }
+
+        findViewById<Button>(R.id.video_button).setOnClickListener {
+            recordVideo()
+        }
+    }
+
+    private fun takePicture() {
+        val time = Date().time
+        latestTmpUri = providerManager.generatePhotoUri(time)
+        latestTmpUri?.let {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, it.uri)
+            takePicture.launch(takePictureIntent)
+        }
+    }
+
+    private fun recordVideo() {
+        val time = Date().time
+        latestTmpUri = providerManager.generateVideoUri(time)
+        latestTmpUri?.let {
+            val recordVideoIntent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
+            recordVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, it.uri)
+            recordVideo.launch(recordVideoIntent)
         }
     }
 }
